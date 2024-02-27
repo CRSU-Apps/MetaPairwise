2e3e656e1997982987af241c9265ed0b6d091553
JanionNevill <janion.nevill@leicester.ac.uk>
2024-02-27 12:47:50 +0000

    Updated treatment and comparator selection to be more robust. More work to follow.

--- R/optionsPanel.R
+++ R/optionsPanel.R
@@ -1,33 +1,114 @@
 optionsPanelUI <- function(id) {
-  ns=NS(id)
-  div(h4("Synthesis Options"),
-               
-               fluidRow(column(6,selectInput(inputId = ns("Pair_Trt"), label = "Select Treatment", choices = NULL)
-               ),
-               column(6,selectInput(inputId = ns("Pair_Ctrl"), label = "Select Comparator", choices = NULL)
-               )
-               ),
-               radioButtons(ns("FixRand"), "Model selection:", c("Fixed-effects model (FE)" = "fixed", "Random-effects model (RE)" = "random")
-               ),
-               conditionalPanel(condition = "output.ContBin=='continuous'",
-                                radioButtons(ns("OutcomeCont"), "Outcome for continuous data:", c("Mean Difference (MD)" = "MD","Standardised Mean Difference (SMD)" = "SMD")
-                                )
-               ),
-               conditionalPanel(condition = "output.ContBin=='binary'",
-                                radioButtons(ns("OutcomeBina"), "Outcome for binary data:", c("Odds Ratio (OR)" = "OR","Risk Ratio (RR)" = "RR", "Risk Difference (RD)" = "RD")
-                                )
-               ),
-               conditionalPanel(condition= "input.AnalysisType=='Bayesian Analysis'",
-                                h5("Bayesian Options"),
-                                fluidRow(column(6, radioButtons(ns("prior"), "Vague prior for between study standard deviation:", c("Half-Cauchy(0,0.5)" = "half-cauchy", "Uniform(0,2)" = "uniform", "Half-Normal(0,1)" = "half-normal")),
-                                                actionButton(ns("bayes_help"), "Help", class="btn-xs", style="position: absolute; left: 0; top: 220px")
-                                ),
-                                column(6, numericInput(ns("chains"), "Number of chains:", value=2, min=1),
-                                       numericInput(ns("iter"), "Number of iterations:", value=4000, min=1),
-                                       numericInput(ns("burn"), "Burn-in:", value=400, min=1)
-                                )
-                                )
-               )
+  ns = NS(id)
+  div(
+    h4("Synthesis Options"),
+    fluidRow(
+      div(
+        style = "display: flex; justify-content: space-between; margin: 0 15px",
+        selectInput(
+          inputId = ns("Pair_Trt"),
+          label = "Select Treatment",
+          choices = NULL,
+          selectize = FALSE
+        ),
+        div(
+          style = "display: inline-block; align-self: center;",
+          actionButton(
+            inputId = ns("swap_treatments"),
+            label = NULL,
+            icon = icon(name = "arrow-right-arrow-left")
+          )
+        ),
+        selectInput(
+          inputId = ns("Pair_Ctrl"),
+          label = "Select Comparator",
+          choices = NULL,
+          selectize = FALSE
+        )
+      )
+    ),
+    radioButtons(
+      inputId = ns("FixRand"),
+      label = "Model selection:",
+      choices = c(
+        "Fixed-effects model (FE)" = "fixed",
+        "Random-effects model (RE)" = "random"
+      )
+    ),
+    # Intentionally not namespaced in order to capture values from global environment.
+    # This is not the right way to do this. It should be fixed in future
+    conditionalPanel(
+      condition = "output.ContBin == 'continuous'",
+      radioButtons(
+        inputId = ns("OutcomeCont"),
+        label = "Outcome for continuous data:",
+        choices = c(
+          "Mean Difference (MD)" = "MD",
+          "Standardised Mean Difference (SMD)" = "SMD"
+        )
+      )
+    ),
+    # Intentionally not namespaced in order to capture values from global environment.
+    # This is not the right way to do this. It should be fixed in future
+    conditionalPanel(
+      condition = "output.ContBin == 'binary'",
+      radioButtons(
+        inputId = ns("OutcomeBina"),
+        label = "Outcome for binary data:",
+        choices = c(
+          "Odds Ratio (OR)" = "OR",
+          "Risk Ratio (RR)" = "RR",
+          "Risk Difference (RD)" = "RD"
+        )
+      )
+    ),
+    # Intentionally not namespaced in order to capture values from global environment.
+    # This is not the right way to do this. It should be fixed in future
+    conditionalPanel(
+      condition= "input.AnalysisType == 'Bayesian Analysis'",
+      h5("Bayesian Options"),
+      fluidRow(
+        column(
+          width = 6,
+          radioButtons(
+            inputId = ns("prior"),
+            label = "Vague prior for between study standard deviation:",
+            choices = c(
+              "Half-Cauchy(0,0.5)" = "half-cauchy",
+              "Uniform(0,2)" = "uniform",
+              "Half-Normal(0,1)" = "half-normal"
+            )
+          ),
+          actionButton(
+            inputId = ns("bayes_help"),
+            label = "Help",
+            class = "btn-xs",
+            style = "position: absolute; left: 0; top: 220px"
+          )
+        ),
+        column(
+          width = 6,
+          numericInput(
+            inputId = ns("chains"),
+            label = "Number of chains:",
+            value = 2,
+            min = 1
+          ),
+          numericInput(
+            inputId = ns("iter"),
+            label = "Number of iterations:",
+            value = 4000,
+            min = 1
+          ),
+          numericInput(
+            inputId = ns("burn"),
+            label = "Burn-in:",
+            value = 400,
+            min = 1
+          )
+        )
+      )
+    )
   )
 }
 
@@ -36,27 +117,43 @@ optionsPanelServer <- function(id, data) {
     id,
     function(input, output, session) {
       
-      ns <- session$ns
+      # Update available treatments when the data changes
+      observe({
+        updateSelectInput(inputId = "Pair_Trt", choices = data()$levels, selected = data()$levels[1])
+      }) %>% bindEvent(data())
       
-      pairwise_ref <- function(trt_ctrl) {   # pairwise options
-        if (trt_ctrl=='trt') {
-          ref <- reactive({
-              return(data()$levels[1])
-          })
-        } else {
-          ref <- reactive({
-              return(data()$levels[2])
-          })
+      # Update available comparators when treatment changes
+      observe({
+        items <- data()$levels
+        items <- items[items != input$Pair_Trt]
+        
+        selected <- input$Pair_Ctrl
+        if (is.null(selected) || !selected %in% items) {
+          selected <- items[1]
         }
-        return(ref())
-      }
+        updateSelectInput(inputId = "Pair_Ctrl", choices = items, selected = selected)
+        
+        # Allow the comparator selection to be updated given the new choices.
+        ctrl_observer$resume()
+      }) %>% bindEvent(input$Pair_Trt)
       
+      # Used to swap treatments
+      new_ctrl <- reactiveVal()
+      
+      # Swap treatments when button clicked
       observe({
-        updateSelectInput(session=session, inputId = "Pair_Trt", choices = data()$levels, selected = pairwise_ref(trt_ctrl='trt'))
-      })
-      observe({
-        updateSelectInput(session=session, inputId = "Pair_Ctrl", choices = data()$levels, selected = pairwise_ref(trt_ctrl='ctrl'))
-      })
+        new_trt <- input$Pair_Ctrl
+        # Prevent the comparator selection from updating until after the choices have been updated.
+        ctrl_observer$suspend()
+        new_ctrl(input$Pair_Trt)
+        updateSelectInput(inputId = "Pair_Trt", selected = new_trt)
+      }) %>% bindEvent(input$swap_treatments)
+      
+      # Swap treatments when button clicked
+      ctrl_observer <- observe({
+        updateSelectInput(inputId = "Pair_Ctrl", selected = new_ctrl())
+        new_ctrl(NULL)
+      }) %>% bindEvent(new_ctrl(), ignoreNULL = TRUE)
       
       # Interactive help boxes #
       
