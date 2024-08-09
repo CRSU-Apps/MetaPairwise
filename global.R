5cd06f927d2ea8643fcd78057a9b37934dc45ca0
JanionNevill <janion.nevill@leicester.ac.uk>
2024-08-09 09:12:36 +0100

    Automated installation of packages

--- global.R
+++ global.R
@@ -11,23 +11,30 @@ library(shinythemes)
 library(shinyWidgets)
 
 MetaLoadLibraries <- shinymeta::metaAction({
-  library(dplyr)
-  library(forcats)
-  library(ggplot2)
-  library(glue)
-  library(markdown)
-  library(metafor)
-  library(MetaStan)
-  library(purrr)
-  library(rintrojs)
-  library(rio)
-  library(rmarkdown)
-  library(rstan)
-  library(tidybayes)
-  library(tidyr)
-  library(tidyverse)
+  LoadOrInstall <- function(package_name) {
+    if (!require(package_name, character.only = TRUE)) {
+      install.packages(package_name)
+      library(package_name, character.only = TRUE)
+    }
+  }
+  
+  LoadOrInstall("dplyr")
+  LoadOrInstall("forcats")
+  LoadOrInstall("ggplot2")
+  LoadOrInstall("glue")
+  LoadOrInstall("markdown")
+  LoadOrInstall("metafor")
+  LoadOrInstall("MetaStan")
+  LoadOrInstall("purrr")
+  LoadOrInstall("rintrojs")
+  LoadOrInstall("rio")
+  LoadOrInstall("rmarkdown")
+  LoadOrInstall("rstan")
+  LoadOrInstall("tidybayes")
+  LoadOrInstall("tidyr")
+  LoadOrInstall("tidyverse")
   
   "# Source all files in 'R' directory"
-  library(R.utils)
+  LoadOrInstall("R.utils")
   R.utils::sourceDirectory(path = "R", modifiedOnly = FALSE)
 })
