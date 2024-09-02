cce257ecda4726e4643836a43a0c75861935be33
JanionNevill <janion.nevill@leicester.ac.uk>
2024-09-02 11:43:25 +0100

    Changed file exports to temporary directories

--- tests/testthat/test-export.R
+++ tests/testthat/test-export.R
@@ -7,13 +7,17 @@ test_that("ExportFrequentistJson() binary data regression test", {
     SwapTrt(CONBI = "binary", trt = "BEVA") |>
     FreqPair("OR", "binary", "both")
   
-  readr::write_file(json, "data/frequentist_binary_or.json")
+  json <- ExportFrequentistJson(binary_ma, "random", "OR")
+  
+  temp_dir <- tempdir()
+  file_path <- file.path(temp_dir, "frequentist_binary_or.json")
+  readr::write_file(json, file_path)
   
   expect_equal(
     as.character(json),
     stringr::str_trim(
       stringr::str_replace_all(
-        readr::read_file("data/frequentist_binary_or.json"),
+        readr::read_file(file_path),
         "\r\n",
         "\n"
       )
@@ -30,12 +34,15 @@ test_that("ExportFrequentistJson() continuous data regression test", {
     FreqPair("MD", "continuous", "both")
   
   json <- ExportFrequentistJson(continuous_ma, "random", "MD")
+  temp_dir <- tempdir()
+  file_path <- file.path(temp_dir, "frequentist_continuous_md.json")
+  readr::write_file(json, file_path)
   
   expect_equal(
     as.character(json),
     stringr::str_trim(
       stringr::str_replace_all(
-        readr::read_file("data/frequentist_continuous_md.json"),
+        readr::read_file(file_path),
         "\r\n",
         "\n"
       )
@@ -51,12 +58,15 @@ test_that("ExportBayesianJson() binary data regression test", {
     FreqPair("OR", "binary", "both")
   
   json <- ExportBayesianJson(binary_ma, "random", "OR")
+  temp_dir <- tempdir()
+  file_path <- file.path(temp_dir, "bayesian_binary_or.json")
+  readr::write_file(json, file_path)
   
   expect_equal(
     as.character(json),
     stringr::str_trim(
       stringr::str_replace_all(
-        readr::read_file("data/bayesian_binary_or.json"),
+        readr::read_file(file_path),
         "\r\n",
         "\n"
       )
@@ -73,12 +83,15 @@ test_that("ExportBayesianJson() continuous data regression test", {
     FreqPair("MD", "continuous", "both")
   
   json <- ExportBayesianJson(continuous_ma, "random", "MD")
+  temp_dir <- tempdir()
+  file_path <- file.path(temp_dir, "bayesian_continuous_md.json")
+  readr::write_file(json, file_path)
   
   expect_equal(
     as.character(json),
     stringr::str_trim(
       stringr::str_replace_all(
-        readr::read_file("data/bayesian_continuous_md.json"),
+        readr::read_file(file_path),
         "\r\n",
         "\n"
       )
